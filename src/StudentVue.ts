5bca99c36a584012d73bde86259f12699396d011
Kai Chang <kaijchang@gmail.com>
2020-03-31 12:08:41 -0700

    handle more routes and update demo

--- src/StudentVue.ts
+++ src/StudentVue.ts
@@ -9,6 +9,7 @@ const CONFIG = require('../config.json');
 const TEMPLATES_PATH = path.join(__dirname, '..', 'templates');
 
 const TestWebServiceURL = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'TestWebServiceURL.xml'), 'utf8'));
+const AckStatement = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'AckStatement.xml'), 'utf8'));
 const ChildList = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'ChildList.xml'), 'utf8'));
 const SoundFileData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'SoundFileData.xml'), 'utf8'));
 const LanguageLists = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'LanguageLists.xml'), 'utf8'));
@@ -17,23 +18,22 @@ const AllModuleRecordData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH,
 const PXPMessagesData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'PXPMessages.xml'), 'utf8'));
 
 const StudentVue: { [key: string]: (params: object) => any } = {
-    // MISC METHODS
+    SHOW_GET_FORGOT_PASSWORD_BUTTON_STATUS: () => true,
+    GETSECURITYSTATUS: () => false,
+    GetActivationLinkStatus_StudentVUE: () => true,
+    GETACKTEXT: () => {
+        return AckStatement({
+            privacyStatement: 'This is a test privacy statement.'
+        });
+    },
+    LOGOUT: () => null,
+
     TestWebServiceURL: () => {
         return TestWebServiceURL({
             organizationName: CONFIG.organizationName
         });
     },
-    SHOW_GET_FORGOT_PASSWORD_BUTTON_STATUS: () => true,
-    GETSECURITYSTATUS: () => false,
-    ChildList: () => {
-        return ChildList({
-            districtEvents: [{DstEventGU: '', EventDate: moment().format('MM/DD/YYYY'), EventTime: '', ShortTitle: 'Test', ShortDesc: 'This is a test StudentVue server, behavior may be unpredictable.'}],
-            childName: 'Test User',
-            childID: 12345678,
-            organizationName: CONFIG.organizationName,
-            childGrade: 12
-        });
-    },
+
     GetSoundFileData: () => {
         return SoundFileData();
     },
@@ -44,7 +44,15 @@ const StudentVue: { [key: string]: (params: object) => any } = {
         return AllModuleRecordData();
     },
 
-    // DATA METHODS
+    ChildList: () => {
+        return ChildList({
+            districtEvents: [{DstEventGU: '', EventDate: moment().format('MM/DD/YYYY'), EventTime: '', ShortTitle: 'Test', ShortDesc: 'This is a test StudentVue server, behavior may be unpredictable.'}],
+            childName: 'Test User',
+            childID: 12345678,
+            organizationName: CONFIG.organizationName,
+            childGrade: 12
+        });
+    },
 
     GetPXPMessages: () => {
         return PXPMessagesData({
