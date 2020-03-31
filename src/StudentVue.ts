93649c64591d108cc7ce127c7a0b6a3c5cda0064
Kai Chang <kaijchang@gmail.com>
2020-03-31 11:28:24 -0700

    working example

--- src/StudentVue.ts
+++ src/StudentVue.ts
@@ -4,37 +4,50 @@ import * as path from 'path';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 
+const CONFIG = require('../config.json');
+
 const TEMPLATES_PATH = path.join(__dirname, '..', 'templates');
 
-const PXPMessages = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'PXPMessages.xml'), 'utf8'));
+const TestWebServiceURL = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'TestWebServiceURL.xml'), 'utf8'));
 const ChildList = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'ChildList.xml'), 'utf8'));
 const SoundFileData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'SoundFileData.xml'), 'utf8'));
-const SupportedLanguages = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'SupportedLanguages.xml'), 'utf8'));
-const ContentUserDefinedModule = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'ContentUserDefinedModule.xml'), 'utf8'));
+const LanguageLists = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'LanguageLists.xml'), 'utf8'));
+const AllModuleRecordData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'AllModuleRecordData.xml'), 'utf8'));
+
+const PXPMessagesData = _.template(fs.readFileSync(path.join(TEMPLATES_PATH, 'PXPMessages.xml'), 'utf8'));
 
 const StudentVue: { [key: string]: (params: object) => any } = {
     // MISC METHODS
+    TestWebServiceURL: () => {
+        return TestWebServiceURL({
+            organizationName: CONFIG.organizationName
+        });
+    },
     SHOW_GET_FORGOT_PASSWORD_BUTTON_STATUS: () => true,
     GETSECURITYSTATUS: () => false,
     ChildList: () => {
         return ChildList({
-            date: moment().format('MM/DD/YYYY')
+            districtEvents: [{DstEventGU: '', EventDate: moment().format('MM/DD/YYYY'), EventTime: '', ShortTitle: 'Test', ShortDesc: 'This is a mock StudentVue server, behavior may be unpredictable.'}],
+            childName: 'Test User',
+            childID: 12345678,
+            organizationName: CONFIG.organizationName,
+            childGrade: 12
         });
     },
     GetSoundFileData: () => {
         return SoundFileData();
     },
     GetSupportedLanguages: () => {
-        return SupportedLanguages();
+        return LanguageLists();
     },
     GetContentUserDefinedModule: () => {
-        return ContentUserDefinedModule();
+        return AllModuleRecordData();
     },
 
     // DATA METHODS
 
     GetPXPMessages: () => {
-        return PXPMessages({
+        return PXPMessagesData({
             messages: [
                 {IconURL: 'images/PXP/TchComment_S.gif', ID: 'Test Message ID', BeginDate: moment().format('MM/DD/YYYY kk:mm:ss'), Type: 'StudentActivity', Subject: 'This is a Test Message', Content: 'This is a Test Message.'}
             ]
